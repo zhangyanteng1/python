d93e2ee3e2b6b437eb0b7c4adfa2b0abdd0b9d48
unknown <zhangyanteng@baijia.com>
2021-12-28 11:20:30 +0800

    uodate

--- scripts/test_pp.py
+++ scripts/test_pp.py
@@ -2,12 +2,22 @@ import pytest
 from selenium import webdriver
 from time import sleep
 import allure
+
+
 class TestGaoTu:
 
     @allure.step(title='高途')
     def test_gaotu(self):
         driver = webdriver.Chrome()
         driver.get("http://www.gaotu100.com")
+        driver.maximize_window()
+        sleep(3)
+        driver.quit()
+
+    @allure.step(title='百度')
+    def test_baidu(self):
+        driver = webdriver.Chrome()
+        driver.get("http://www.baidu.com")
         driver.maximize_window()
         sleep(3)
         driver.quit()
